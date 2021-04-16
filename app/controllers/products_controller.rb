809ecacb1ce950e74ff9e2fc0aa77db8c5affa23
Ryan Erricson <ryanerricson@gmail.com>
2021-04-16 11:29:20 -0400

    commit tree ce864a2e64314606aaa7345eff5d9c9f7560f6a5

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+class ProductsController < ApplicationController
+
+    helper_method :cart
+
+    def index
+        
+    end
+
+    def add
+        cart << params[:product]    
+        render 'index' 
+    end
+end
